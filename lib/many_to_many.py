3f2a26e1af7f297003962b86a7e9193c4cdc976c
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-03-08 14:04:15 +0300

    test passed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,59 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        self.contracts_list = []
+        self.all.append(self)
+
+    def contracts(self):
+        return self.contracts_list
+
+    def sign_contract(self, book, date, royalties):
+        if not isinstance(book, Book):
+            raise Exception("Invalid book")
+        contract = Contract(self, book, date, royalties)
+        self.contracts_list.append(contract)
+        return contract
+
+    def books(self):
+        return [contract.book for contract in self.contracts_list]
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts_list)
 
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        self.contracts_list = []
+        self.all.append(self)
+
+    def contracts(self):
+        return self.contracts_list
+
+    def authors(self):
+        return [contract.author for contract in self.contracts_list]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Invalid author")
+        if not isinstance(book, Book):
+            raise Exception("Invalid book")
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        self.author.contracts_list.append(self)
+        self.book.contracts_list.append(self)
+        self.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
